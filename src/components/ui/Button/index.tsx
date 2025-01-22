a3492b1acc6761ecd952fcfb35a84e943bb8fd75
Mike Newberry <hello@mikenew.xyz>
2025-01-22 18:53:33 +0000

    fix(ui-components): prevent non-bordered buttons from resizing on focus

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -8,6 +8,8 @@ import { Button as ButtonAria, Link as LinkAria, composeRenderProps } from "reac
 import { cva } from "class-variance-authority";
 import cn from "@/utils/cn";
 
+const outlineFocusClasses = "focus-visible:outline-dashed focus-visible:outline-primary";
+
 const buttonVariants = cva(
   [
     "inline-flex items-center gap-2 text-primary border-2 border-primary w-fit py-0.5 px-2.5 text-base uppercase transition-transform duration-700",
@@ -15,13 +17,14 @@ const buttonVariants = cva(
     "hover:scale-95 hover:transition-transform hover:duration-700",
     "pressed:scale-90 pressed:duration-200",
     "disabled:opacity-50 disabled:line-through transition-opacity",
+    "outline-primary",
   ],
   {
     variants: {
       variant: {
         default: "bg-secondary focus:border-dashed hover:border-dashed",
-        ghost: "border-none focus-visible:border-dashed",
-        icon: "border-none p-0.5 focus-visible:border-dashed",
+        ghost: `border-none focus-visible:outline-2 ${outlineFocusClasses}`,
+        icon: `border-none p-0.5 focus-visible:outline-2 ${outlineFocusClasses}`,
       },
     },
     defaultVariants: {
